7ce7a3cc2fe63c43a69b6caa1cb9658549c1a64f
Junior Vieira <junior.vieira.1990@gmail.com>
2022-05-23 23:52:21 -0300

    ParkingSpotModel

---
+++ src/main/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,60 @@
+package com.api.parkingcontrol.models;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class ParkingSpotModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NonNull
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+
+    @NonNull
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+
+    @NonNull
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+    @NonNull
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @NonNull
+    @Column(nullable = false, length = 30)
+    private String block;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ParkingSpotModel that = (ParkingSpotModel) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
